10cd3cad47728be853449422a631b4f61a6e4078
CO <99570414+codeFromCO@users.noreply.github.com>
2023-06-17 16:10:14 -0400

    end-of-sprint

--- client/components/Card.js
+++ client/components/Card.js
@@ -1,32 +1,17 @@
-// import React from 'react';
-
-// import '../style.css'
-
-// const Card = () => {
-//   return (
-//     <div className='Card'>
-//       <h1>Name</h1>
-//       <h2>Created by: </h2>
-//       <ul>
-//         <li>Count: </li>
-//         <li>Patricipiants: </li>
-//       </ul>
-//     </div>
-//   );
-// };
-
-// export default Card;
 import React from "react";
 const Card = ({ activityName, currentCount, requiredCount, owner }) => {
   return (
+    <div className="activityCard">
     <div className='Card'>
-      <h1>{activityName}</h1>
-      <h2>Created by: {owner}</h2>
+      <h2>{activityName}</h2>
+      <hr></hr>
+      <h3>Created by: {owner}</h3>
       <ul>
         <li>Count: {currentCount}</li>
         <li>Required participants: {requiredCount}</li>
       </ul>
       <button>Join</button>
+    </div>
     </div>
   );
 };
